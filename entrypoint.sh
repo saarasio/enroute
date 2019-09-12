0c1a1018198a27df569913de136d4212873b052d
chintan8saaras <chintan8saaras@saaras.io>
2019-09-12 01:40:32 +0000

    Dump value of ENROUTE_CP_PROTO when starting up.
    Related to #29

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,16 @@ else
 	echo "\$ENROUTE_CP_PORT set to $ENROUTE_CP_PORT"
 fi
 
+if [ -z "$ENROUTE_CP_PROTO" ]
+then
+	echo "\$ENROUTE_CP_PROTO is not set. Please set before running"
+	exit	1
+else
+	echo "\$ENROUTE_CP_PROTO set to $ENROUTE_CP_PROTO"
+fi
+
+
+
 /enroute/enroute serve --xds-port=8001 --xds-address=127.0.0.1 --enroute-cp-ip $ENROUTE_CP_IP --enroute-cp-port $ENROUTE_CP_PORT --enroute-cp-proto $ENROUTE_CP_PROTO --enroute-name $ENROUTE_NAME &
 sleep 5
 /usr/local/bin/envoy -c /enroute/config.json --service-node "service-node" --service-cluster "$ENROUTE_NAME" --log-level trace 
