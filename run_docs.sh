47950ba329055646226e13b7ab4df1a3ff9e6574
chintan8saaras <chintan8saaras@saaras.io>
2019-09-24 00:28:52 +0000

    Update run_docs command

--- run_docs.sh
+++ run_docs.sh
@@ -1,1 +1,2 @@
-cd documentation/enroute && hugo serve -D --verbose  -b http://docs.ingresspipe.io:8080/ --appendPort=false --debug --disableFastRender -w --log --ignoreCache
+#cd documentation/enroute && hugo serve -D --verbose  -b http://docs.ingresspipe.io:8080/ --appendPort=false --debug --disableFastRender -w --log --ignoreCache
+cd documentation/enroute && hugo serve -D --verbose  -b https://docs.ingresspipe.io:8443/ --appendPort=false --debug --disableFastRender -w --log --ignoreCache
