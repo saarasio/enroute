295f9f1d757df867533461ea0b8bdcba171b08a7
chintan8saaras <chintan8saaras@saaras.io>
2019-10-04 01:21:46 +0000

    Add commands to run new dp/cp

---
+++ documentation/run_new_cp.sh
@@ -0,0 +1,1 @@
+docker run -p 80:8080 -p 1323:1323 gcr.io/enroute-cp:latest
---
+++ documentation/run_new_dp.sh
@@ -0,0 +1,1 @@
+docker run -e ENROUTE_NAME=test -e ENROUTE_CP_IP=127.0.0.1 -e ENROUTE_CP_PORT=80 -e ENROUTE_CP_PROTO=http --net=host gcr.io/enroute-dp
