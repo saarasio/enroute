4c4946d6650e87e972ed6a45c4ef882d00ef6bbe
Chintan <chintan8saaras@saaras.io>
2019-10-22 03:53:50 +0000

    Fix dp script port, refactor CMakeLists.txt

--- documentation/run_new_dp.sh
+++ documentation/run_new_dp.sh
@@ -1,1 +1,1 @@
-docker run -e ENROUTE_NAME=testproxy -e ENROUTE_CP_IP=127.0.0.1 -e ENROUTE_CP_PORT=8080 -e ENROUTE_CP_PROTO=http --net=host gcr.io/enroute-10102020/enroute-dp
+docker run -e ENROUTE_NAME=testproxy -e ENROUTE_CP_IP=127.0.0.1 -e ENROUTE_CP_PORT=8888 -e ENROUTE_CP_PROTO=http --net=host gcr.io/enroute-10102020/enroute-dp
