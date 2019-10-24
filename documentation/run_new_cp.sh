38fa04a7465d4dddab73d075dfe5fa5ab63dcf35
Chintan <chintan8saaras@saaras.io>
2019-10-24 22:30:48 +0000

    Use 8888 instead of 8080 as hasura listen port

--- documentation/run_new_cp.sh
+++ documentation/run_new_cp.sh
@@ -1,3 +1,3 @@
 # Admin API port (1323 -> 1323) - Run config against this port
 # Enroute control plane port (8888 -> 8080) - Provide this port when bootstrapping data plane
-docker run -v db_data:/var/lib/postgresql/11/main -p 8888:8080 -p 1323:1323 -e WEBAPP_SECRET="treeseverywhere" gcr.io/enroute-10102020/enroute-cp
+docker run -v db_data:/var/lib/postgresql/11/main -p 8888:8888 -p 1323:1323 -e WEBAPP_SECRET="treehugger" gcr.io/enroute-10102020/enroute-cp
