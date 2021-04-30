633269bad052fb0cf544f983caa0fb1a88292f58
chintan <chintan8saaras@saaras.io>
2021-04-30 23:03:12 +0000

    go fmt ./...

--- enroute-dp/internal/envoy/route_filter.go
+++ enroute-dp/internal/envoy/route_filter.go
@@ -25,7 +25,7 @@ var test_cors_config = `
 }
 `
 
-func SaarasConfigToEnvoyConfig(cfg *cfg.CorsFilterConfig) *envoy_config_route_v3.CorsPolicy{
+func SaarasConfigToEnvoyConfig(cfg *cfg.CorsFilterConfig) *envoy_config_route_v3.CorsPolicy {
 
 	if cfg == nil {
 		return nil
@@ -34,68 +34,68 @@ func SaarasConfigToEnvoyConfig(cfg *cfg.CorsFilterConfig) *envoy_config_route_v3
 	smlist := make([]*envoy_type_matcher_v3.StringMatcher, 0)
 
 	if len(cfg.MatchCondition.Regex) > 0 {
-			sm := &envoy_type_matcher_v3.StringMatcher{
-					MatchPattern: &envoy_type_matcher_v3.StringMatcher_SafeRegex{
-							SafeRegex: &envoy_type_matcher_v3.RegexMatcher{
-									EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{
-											GoogleRe2: &envoy_type_matcher_v3.RegexMatcher_GoogleRE2{},
-									},
-									Regex: cfg.MatchCondition.Regex,
-							},
+		sm := &envoy_type_matcher_v3.StringMatcher{
+			MatchPattern: &envoy_type_matcher_v3.StringMatcher_SafeRegex{
+				SafeRegex: &envoy_type_matcher_v3.RegexMatcher{
+					EngineType: &envoy_type_matcher_v3.RegexMatcher_GoogleRe2{
+						GoogleRe2: &envoy_type_matcher_v3.RegexMatcher_GoogleRE2{},
 					},
-			}
+					Regex: cfg.MatchCondition.Regex,
+				},
+			},
+		}
 
-			smlist = append(smlist, sm)
+		smlist = append(smlist, sm)
 	}
 
 	if len(cfg.MatchCondition.Prefix) > 0 {
-			sm := &envoy_type_matcher_v3.StringMatcher{
-					MatchPattern: &envoy_type_matcher_v3.StringMatcher_Prefix{
-						Prefix: cfg.MatchCondition.Prefix,
-					},
-			}
-			smlist = append(smlist, sm)
+		sm := &envoy_type_matcher_v3.StringMatcher{
+			MatchPattern: &envoy_type_matcher_v3.StringMatcher_Prefix{
+				Prefix: cfg.MatchCondition.Prefix,
+			},
+		}
+		smlist = append(smlist, sm)
 	}
 
 	if len(cfg.MatchCondition.Exact) > 0 {
-			sm := &envoy_type_matcher_v3.StringMatcher{
-					MatchPattern: &envoy_type_matcher_v3.StringMatcher_Exact{
-						Exact:cfg.MatchCondition.Exact,
-					},
-			}
-			smlist = append(smlist, sm)
+		sm := &envoy_type_matcher_v3.StringMatcher{
+			MatchPattern: &envoy_type_matcher_v3.StringMatcher_Exact{
+				Exact: cfg.MatchCondition.Exact,
+			},
+		}
+		smlist = append(smlist, sm)
 	}
 
 	if len(cfg.MatchCondition.Suffix) > 0 {
-			sm := &envoy_type_matcher_v3.StringMatcher{
-					MatchPattern: &envoy_type_matcher_v3.StringMatcher_Suffix{
-						Suffix: cfg.MatchCondition.Suffix,
-					},
-			}
-			smlist = append(smlist, sm)
+		sm := &envoy_type_matcher_v3.StringMatcher{
+			MatchPattern: &envoy_type_matcher_v3.StringMatcher_Suffix{
+				Suffix: cfg.MatchCondition.Suffix,
+			},
+		}
+		smlist = append(smlist, sm)
 	}
 
 	if len(cfg.MatchCondition.Contains) > 0 {
-			sm := &envoy_type_matcher_v3.StringMatcher{
-					MatchPattern: &envoy_type_matcher_v3.StringMatcher_Contains{
-							Contains: cfg.MatchCondition.Contains,
-					},
-			}
-			smlist = append(smlist, sm)
+		sm := &envoy_type_matcher_v3.StringMatcher{
+			MatchPattern: &envoy_type_matcher_v3.StringMatcher_Contains{
+				Contains: cfg.MatchCondition.Contains,
+			},
+		}
+		smlist = append(smlist, sm)
 	}
 
 	c_cfg := envoy_config_route_v3.CorsPolicy{
 		AllowOriginStringMatch: smlist,
-		AllowMethods: cfg.AccessControlAllowMethods,
-		AllowHeaders: cfg.AccessControlAllowHeaders,
-		ExposeHeaders: cfg.AccessControlExposeHeaders,
-		MaxAge: cfg.AccessControlMaxAge,
+		AllowMethods:           cfg.AccessControlAllowMethods,
+		AllowHeaders:           cfg.AccessControlAllowHeaders,
+		ExposeHeaders:          cfg.AccessControlExposeHeaders,
+		MaxAge:                 cfg.AccessControlMaxAge,
 	}
 
 	return &c_cfg
 }
 
-func corsConfig(vh *dag.VirtualHost) *envoy_config_route_v3.CorsPolicy{
+func corsConfig(vh *dag.VirtualHost) *envoy_config_route_v3.CorsPolicy {
 
 	if vh != nil {
 		fmt.Printf("envoy:corsConfig() VH [%+v]\n", vh)
@@ -104,8 +104,8 @@ func corsConfig(vh *dag.VirtualHost) *envoy_config_route_v3.CorsPolicy{
 
 		// No CORS filter
 		if cors_http_filter == nil {
-				fmt.Printf("envoy:corsConfig(): No cors filter found \n")
-				return nil
+			fmt.Printf("envoy:corsConfig(): No cors filter found \n")
+			return nil
 		}
 
 		filter_config := cors_http_filter.Filter.Filter_config
@@ -115,8 +115,8 @@ func corsConfig(vh *dag.VirtualHost) *envoy_config_route_v3.CorsPolicy{
 		unmsh_cfg, e := cfg.UnmarshalCorsFilterConfig(filter_config)
 
 		if e != nil {
-						fmt.Printf("envoy:corsConfig() Failed to Unmarshal cors config \n")
-						return nil
+			fmt.Printf("envoy:corsConfig() Failed to Unmarshal cors config \n")
+			return nil
 		}
 
 		fmt.Printf("envoy:corsConfig() Unmarshalled Filter Config [%+v]\n", unmsh_cfg)
@@ -130,7 +130,6 @@ func corsConfig(vh *dag.VirtualHost) *envoy_config_route_v3.CorsPolicy{
 
 	return nil
 }
-
 
 func SetupEnvoyFilters(vh *dag.VirtualHost, vhost *envoy_config_route_v3.VirtualHost, isVh bool, r *dag.Route) {
 	c := corsConfig(vh)
