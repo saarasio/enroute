68ff074c739e010c964f7a7fd038e50ffd5f6ddf
chintan8saaras <chintan8saaras@saaras.io>
2022-06-06 22:06:11 +0000

    Add circuitbreaker test
    Ref: 5327a6b110eedb3f1c32ea7643ee5646156897a2

--- enroute-dp/internal/envoy/cluster_test.go
+++ enroute-dp/internal/envoy/cluster_test.go
@@ -434,6 +434,63 @@ func TestCluster(t *testing.T) {
 				DnsLookupFamily: envoy_config_cluster_v3.Cluster_V4_ONLY,
 			},
 		},
+		"Verify CircuitBreakers filter": {
+			cluster: &dag.Cluster{
+				Upstream: &dag.TCPService{
+					Name: s1.Name, Namespace: s1.Namespace,
+					ServicePort: &s1.Spec.Ports[0],
+				},
+				HealthCheck: &gatewayhostv1.HealthCheck{
+					Path: "/healthz",
+				},
+				ClusterFilters:[]*dag.RouteFilter{{
+						Filter: dag.Filter{
+							Filter_name: "cb-filter",
+							Filter_type: "route_filter_circuitbreakers",
+							Filter_config: `{
+								"max_connections" : 1024,
+								"max_pending_requests" : 128,
+								"max_requests" : 128,
+								"max_retries" : 8
+							}`,
+						},
+				}},
+			},
+			want: &envoy_config_cluster_v3.Cluster{
+				Name:                 "default/kuard/443/bc862a33ca",
+				AltStatName:          "default_kuard_443",
+				ClusterDiscoveryType: ClusterDiscoveryType(envoy_config_cluster_v3.Cluster_EDS),
+				EdsClusterConfig: &envoy_config_cluster_v3.Cluster_EdsClusterConfig{
+					EdsConfig:   ConfigSource("enroute"),
+					ServiceName: "default/kuard/http",
+				},
+				ConnectTimeout:                      protobuf.Duration(250 * time.Millisecond),
+				LbPolicy:                            envoy_config_cluster_v3.Cluster_ROUND_ROBIN,
+				CircuitBreakers: &envoy_config_cluster_v3.CircuitBreakers{
+					Thresholds: []*envoy_config_cluster_v3.CircuitBreakers_Thresholds{{
+						MaxConnections: protobuf.UInt32(1024),
+						MaxPendingRequests: protobuf.UInt32(128),
+						MaxRequests: protobuf.UInt32(128),
+						MaxRetries: protobuf.UInt32(8),
+					}},
+				},
+				CommonLbConfig:                      ClusterCommonLBConfig(),
+				CloseConnectionsOnHostHealthFailure: true,
+				HealthChecks: []*envoy_config_core_v3.HealthCheck{{
+					Timeout:            durationOrDefault(0, hcTimeout),
+					Interval:           durationOrDefault(0, hcInterval),
+					UnhealthyThreshold: countOrDefault(0, hcUnhealthyThreshold),
+					HealthyThreshold:   countOrDefault(0, hcHealthyThreshold),
+					HealthChecker: &envoy_config_core_v3.HealthCheck_HttpHealthCheck_{
+						HttpHealthCheck: &envoy_config_core_v3.HealthCheck_HttpHealthCheck{
+							Host: hcHost,
+							Path: "/healthz",
+						},
+					},
+				}},
+				DnsLookupFamily: envoy_config_cluster_v3.Cluster_V4_ONLY,
+			},
+		},
 	}
 
 	for name, tc := range tests {
